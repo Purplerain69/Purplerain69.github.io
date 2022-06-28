5223f8d0416b3eee7a9078b896b86f1dccda1e51
Alexander <mailonywilo@gmail.com>
2022-06-27 21:26:37 -0400

    new function: output.error()

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import output from "./console.js";
 
 function convert() {
     let string = document.getElementById("input").value
@@ -5,28 +6,20 @@ function convert() {
     while(string.match("console")){
         string = string.replace("console", "output")
     }
-    
     return string
 }
 
 function render() {
-    const $output = document.querySelector(".output__results")    
-
-    const output = {
-        log(args) {
-            $printLine = document.createElement("li");
-            $printLine.classList.add("output-line")
-            $printLine.textContent += args;
-            $output.append($printLine)
-        }
-    }
-    
     const finalCode = convert()
-    eval(finalCode)
-
-
-    const $modal = document.querySelector(".output")
-    $modal.classList.add("output--active")    
+    //call the console (output module)
+    try {
+        eval(finalCode)
+    } catch (error) {
+        output.error(error)
+    } finally {
+        const $modal = document.querySelector(".output")
+        $modal.classList.add("output--active")    
+    }
 }
 
 const $run = document.getElementById("run")
@@ -37,6 +30,7 @@ function hideModal() {
     const $modal = document.querySelector(".output")
     $modal.classList.toggle("output--active")    
 }
+
 
 const $closeBtn = document.querySelector(".close-btn")
 $closeBtn.addEventListener("click", hideModal)
---
+++ console.js
@@ -0,0 +1,15 @@
+export default output = {
+    $output: document.querySelector(".output__results"),
+    log(args) {
+        const $printLine = document.createElement("li");
+        $printLine.classList.add("output-line");
+        $printLine.textContent += args;
+        this.$output.append($printLine)
+    },
+    error(args) {
+        const $printLine = document.createElement("li");
+        $printLine.classList.add("output-line--error");
+        $printLine.textContent += args;
+        this.$output.append($printLine)
+    }
+}
